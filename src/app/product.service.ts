e1d7982daa6c0874ffd3619ff016d7994a03274f
malarsaravana7 <75508395+malarsaravana7@users.noreply.github.com>
2021-07-20 21:43:23 -0700

    nm

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 import 'rxjs/add/operator/map';
+import { Album } from './album';
+
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
@@ -11,6 +14,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json());
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 }
